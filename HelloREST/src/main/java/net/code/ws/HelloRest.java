0b1a5af6bc7314c9205ca82699b16ac9230da0d5
DimitriosMalonas <dimitriosmalonas@outlook.com>
2020-02-09 19:33:30 +0000

    RESTful using Spring Boot prj setup

---
+++ HelloREST/src/main/java/net/code/ws/HelloRest.java
@@ -0,0 +1,18 @@
+package net.code.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloRest {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRest.class, args);
+	}
+
+}
